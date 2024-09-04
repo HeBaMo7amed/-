7566d7a7318ae0cf0de7cc63871ad0c1cfc0784a
rouby <abdullahelrouby750@gmail.com>
2024-09-04 04:05:04 +0300

    added the surah using an api, tested it and handeld the format

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const url = "https://raw.githubusercontent.com/nawafalqari/azkar-api/56df51279ab6eb86dc2f6202c7de26c8948331c1/azkar.json";
+const zekrUrl = "https://raw.githubusercontent.com/nawafalqari/azkar-api/56df51279ab6eb86dc2f6202c7de26c8948331c1/azkar.json";
+const surahUrl = "http://api.alquran.cloud/v1/surah/67" // سورة الملك
+
 const douaaName = {
     Anbiaa : "أدعية الأنبياء",
     Quran : "أدعية قرآنية",
@@ -13,10 +15,10 @@ const douaaName = {
 
 // Fetching data from API
 
-fetch(url)
+fetch(zekrUrl)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        // console.log(data);
         
         x = data[douaaName.MorningZekr][4].content;
         // console.log(x);
@@ -32,6 +34,39 @@ fetch(url)
         test.textContent = x;
         test.style.textAlign = 'center';
         test.style.fontStyle = 'italic';
+        test.classList.add("mb-5", "p-5");
         document.body.appendChild(test);
         
     })
+
+    const spacebtw = document.createElement('br');
+
+fetch(surahUrl)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const ayat = data.data.ayahs;
+        const basmalaTxt = "بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ";
+        let wholeSurah = "";
+        ayat.forEach((ayah, index) => {
+            if (index == 0) ayah.text = ayah.text.replace("بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ", "");
+            wholeSurah += ` ${ayah.text} (${ayah.numberInSurah})`;
+        });
+
+        console.log(wholeSurah);
+        
+        
+        const basmala = document.createElement('h1');
+        basmala.textContent = basmalaTxt;
+        basmala.style.textAlign = 'center';
+        basmala.style.fontStyle = 'italic';
+
+        const surah = document.createElement('h2');
+        surah.textContent = wholeSurah;
+        surah.style.textAlign = 'center';
+        surah.style.fontStyle = 'italic';
+        surah.classList.add("p-5");
+        
+        document.body.appendChild(basmala);
+        document.body.appendChild(surah);
+    });
